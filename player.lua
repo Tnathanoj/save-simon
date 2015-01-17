55cfe58de75cab3ee4d6c9338ce478d01252dc37
Willem Thiart <himself@willemthiart.com>
2015-01-17 15:31:38 +0900

    Added monster collision

--- player.lua
+++ player.lua
@@ -39,6 +39,8 @@ function Player:new()
     o.touching_ground = false
     o.last_jump_time = 0
     o.last_room_change_time = 0
+    o.facing_direction = 1
+    o.weapon_reach = 50
 
     o:new_bbox()
 
@@ -69,11 +71,13 @@ function Player:update(dt)
         self.p = 1
         self.z = 1
         self.current_animation = anims.walking
+        self.facing_direction = 1
     elseif love.keyboard.isDown("left") then
         self.body:applyForce(-self.speed, 0)
         self.p = 1
         self.z = -1
         self.current_animation = anims.walking
+        self.facing_direction = -1
     else
         self.current_animation = anims.standing
     end
@@ -81,13 +85,13 @@ function Player:update(dt)
     if love.keyboard.isDown("z") then
         self.current_animation = anims.attacking
         for k, obj in pairs(current_room.map.layers.Objects.objects) do
-            if obj.type == "monster" then
+            if obj.o and obj.o.takedamage then
                 x,y = self.body:getWorldCenter()
-                d = distance(x, y, obj.x + obj.width/2, obj.y + obj.height/2)
-                if d < 200 then
-                    obj.o.hp = obj.o.hp - 1000
+                d = distance(x + self.facing_direction * self.weapon_reach, y, obj.x + obj.width/2, obj.y + obj.height/2)
+                if d < 50 then
+                    obj.o:takedamage(1000)
                     if obj.o.hp < 0 then
-                        obj.o.body:destroy()
+                        obj.o:kill()
                         current_room.map.layers.Objects.objects[k] = nil
                     end
                 end
@@ -122,10 +126,10 @@ function Player:update(dt)
     if love.keyboard.isDown("up") then
         if self.touching_ground and self.last_jump_time < love.timer.getTime() then
             local jump_power = 2500
-            self.body:applyForce(0, -jump_power)
-            self.body2.body:applyForce(0, -jump_power)
             self.last_jump_time = 1.0 + love.timer.getTime()
             self.touching_ground = false
+            self.body:applyForce(0, -jump_power)
+            self.body2.body:applyForce(0, -jump_power)
         end
     end
 end
